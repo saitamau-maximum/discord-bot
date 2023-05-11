f2bd687c1dffcef20ea4272730917b4e4af3c6cc
kawamurasorachi <sorakawa0218@gmail.com>
2023-05-11 15:49:30 +0900

    feat(deploy): 最新の変更を反映するため、最新のmainをpullするスクリプトを導入

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,10 @@
 
 echo "Deploying..."
 
+# Pull latest changes
+echo "Pulling latest changes..."
+git pull origin main
+
 # Install node dependencies
 echo "Installing node dependencies..."
 npm ci
